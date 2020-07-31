08988d09efe722408b7af8377888227459b4d41b
Thuyvi Nguyen <vivipnguyen@gmail.com>
2020-07-31 14:19:03 -0700

    created react app

--- app/model.py
+++ app/model.py
@@ -41,3 +41,23 @@ class Location(db.Model):
     is_open = Column(db.Boolean, unique=False, default=True)
     latitude = db.Column(db.Integer)
     longitude = db.Column(db.Integer)
+
+##############################################################################
+
+def connect_to_db(app, db_uri="postgresql:///fresh"):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PostgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    from server import app
+    connect_to_db(app)
+    print("Connected to DB.")
