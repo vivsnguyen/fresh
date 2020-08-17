666fe751107a85779a38f6f1f0d601e8b0191a03
Thuyvi Nguyen <vivipnguyen@gmail.com>
2020-08-17 14:52:43 -0700

    added pyc and env to gitignore

--- react-app/api/api.py
+++ react-app/api/api.py
@@ -1,11 +1,8 @@
-from flask import (Flask, render_template, redirect, request, flash, session, jsonify)
+import time
+from flask import (Flask, render_template, redirect, request, flash, session)
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
-def api():
-    return {
-        'userId': 1,
-        'title': 'Flask React Application',
-        'completed': False
-    }
+@app.route("/time", methods=["GET"])
+def get_current_time():
+    return {'time': time.time()}
