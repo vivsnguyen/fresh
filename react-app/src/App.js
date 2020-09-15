b433c8c8d9c29c64aeeec79e5c57e5e87d941ace
Thuyvi Nguyen <vivipnguyen@gmail.com>
2020-09-15 15:25:56 -0700

    added first marker

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,7 @@
 // import React, { Component } from 'react';
 import React from 'react';
-import MapContainer from './components/Map'
+// import MapContainer from './components/Map'
+import MapContainer from './components/Map2'
 
 function App() {
   return (
--- react-app/src/components/Map.js
+++ react-app/src/components/Map.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from "google-maps-react";
+// import React, { Component } from 'react';
+// import { Map, GoogleApiWrapper } from "google-maps-react";
 
-const mapStyles = {
-  width: '100%',
-  height: '70%'
-};
+// const mapStyles = {
+//   width: '100%',
+//   height: '70%'
+// };
 
-export class MapContainer extends Component {
-  render() {
-    return (
-      <Map
-        google={this.props.google}
-        zoom={13}
-        style={mapStyles}
-        initialCenter={{
-         lat: 37.8044,
-         lng: -122.2712
-        }}
-      />
-    );
-  }
-}
+// export class MapContainer extends Component {
+//   render() {
+//     return (
+//       <Map
+//         google={this.props.google}
+//         zoom={13}
+//         style={mapStyles}
+//         initialCenter={{
+//          lat: 37.8044,
+//          lng: -122.2712
+//         }}
+//       />
+//     );
+//   }
+// }
 
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyBTFofRDVmd8HaJzyplpaWhp4_n2RtwSMk'
-})(MapContainer);
+// export default GoogleApiWrapper({
+//   apiKey: 'AIzaSyBTFofRDVmd8HaJzyplpaWhp4_n2RtwSMk'
+// })(MapContainer);
